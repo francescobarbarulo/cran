399cd515f345236c0066203b3f46bd041e67fdda
francesco.barbarulo <barbarulofrancesco@gmail.com>
2018-12-09 23:49:23 +0100

    .gitignore is now working

--- out/clang-release/src/As.d
+++
@@ -1,8 +0,0 @@
-../out/clang-release/src/As.o: As.cc As.h \
-  C:/Users/Gerardo/Desktop/omnet-5.4.1/include\omnetpp.h cranMessage.h
-
-As.h:
-
-C:/Users/Gerardo/Desktop/omnet-5.4.1/include\omnetpp.h:
-
-cranMessage.h:
