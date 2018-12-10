9ea73ed805d7895552385499634ab5a0cc23ec82
gerry94 <g.alvaro@studenti.unipi.it>
2018-12-10 17:25:08 +0100

    Implementazione BBU

--- src/Bbu.cc
+++ src/Bbu.cc
@@ -4,10 +4,37 @@ Define_Module(Bbu);
 
 void Bbu::initialize()
 {
-
+    beep = new cMessage();
+    working = false;
 }
 
 void Bbu::handleMessage(cMessage *msg)
 {
+    if(msg->isSelfMessage()){
+        buffer.erase(buffer.begin());
+        // BBU checks if there are other packet to be transmitted
+        if(buffer.size() == 0)
+            working = false;
+        else{
+            beginTransmission(buffer[0]);
+        }
+    }else{
+        // new packet from AS
+        cranMessage *pkt = check_and_cast<cranMessage*>(msg);
+        buffer.push_back(pkt);
+        if(!working){
+            // Bbu is idle so it can process the packet immediately
+            working = true;
+            beginTransmission(pkt);
+        }
+    }
 
+
+}
+
+void Bbu::beginTransmission(cranMessage *pkt){
+    send(pkt, "out", pkt->getDest());
+    simtime_t transmissionTime = pkt->getSize()/par("speed").doubleValue();
+    EV<<"[BBU] TransmissionTime: "<<transmissionTime<< " - to: "<<pkt->getDest()<<endl;
+    scheduleAt(simTime() + transmissionTime, beep);
 }
--- src/Bbu.h
+++ src/Bbu.h
@@ -9,14 +9,21 @@
 #define BBU_H_
 
 #include <omnetpp.h>
+#include <vector>
+#include "cranMessage.h"
 
 using namespace omnetpp;
 
 class Bbu : public cSimpleModule
 {
+  private:
+    cMessage *beep;
+    std::vector<cranMessage*> buffer;
+    bool working;
   protected:
     virtual void initialize();
     virtual void handleMessage(cMessage *msg);
+    void beginTransmission(cranMessage *pkt);
 };
 
 #endif /* BBU_H_ */
