90d65d7e8b9f040a7f6c0a726b8579278ce50728
francesco.barbarulo <barbarulofrancesco@gmail.com>
2019-01-15 10:31:36 +0100

    script totale

--- scripts/plot.py
+++ scripts/plot.py
@@ -8,14 +8,13 @@ from matplotlib import rcParams
 rcParams['font.family'] = 'serif'
 import matplotlib.pyplot as plt
 
-def parseCommand():
+def parseCommand(numberOfRRH):
     distributions = ['exp', 'log']
-    numberOfRRH = [2, 5, 10]
 
     if len(sys.argv) < 5:
         return None, None
     
-    if sys.argv[1] != '-d' or sys.argv[2] not in distributions or sys.argv[3] != '-n' or int(sys.argv[4]) not in numberOfRRH:
+    if sys.argv[1] != '-d' or sys.argv[2] not in distributions or sys.argv[3] != '-n' or (sys.argv[4] not in str(numberOfRRH) and sys.argv[4] != 'all'):
         return None, None
         
     if sys.argv[2] == 'exp':
@@ -23,19 +22,8 @@ def parseCommand():
 
     return 'lognormal', sys.argv[4]
 
-
-#-------------------#
-#       MAIN        #
-#-------------------#
- 
-def main():
-    d, n = parseCommand()
-    if d is None:
-        print('python plot.ly -d [exp|log] -n [2|5|10]')
-        sys.exit(0)
-            
-    rho = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
-    files = [6667, 3333, 2222, 1667, 1333, 1111, 952, 833, 741]
+def getDataForPlotting(distribution, numberOfRRH):
+    speeds = [6667, 3333, 2222, 1667, 1333, 1111, 952, 833, 741]
 
     z = 2.576 # 99% for CI
     repetitions = 10
@@ -47,11 +35,18 @@ def main():
     n_values = []
     errors = []
 
-    for x in files:
-        filename = '../csv/'+d+'-'+str(x)+'-'+str(n)+'.csv'
+    print('\n*** DISTRIBUTION: %s - N: %s ***' % (distribution, numberOfRRH))
+    print('------------------------------------------')
+
+    for speed in speeds:
+        filename = '../csv/'+distribution+'-'+str(speed)+'-'+str(numberOfRRH)+'.csv'
         print('reading %s ...' % filename)
-        data = pd.read_csv(filename, header=None, usecols=[1, 3, 5, 7, 9, 11, 13, 15, 17, 19], skiprows=1)
-        df.append(pd.DataFrame(data))
+        try:
+            data = pd.read_csv(filename, header=None, usecols=[1, 3, 5, 7, 9, 11, 13, 15, 17, 19], skiprows=1)
+        except:
+            print('Cannot read ' + filename)
+        else:
+            df.append(pd.DataFrame(data))
 
     for i, sheet in enumerate(df):
         print('merge values for df[%d]...' % i)
@@ -67,12 +62,46 @@ def main():
         print('computing mean and stdDev for rho = %.1f...' % ((i+1)*0.1))
         mean_delay_rho.append(stat.mean(x))
         std_dev_delay_rho.append(stat.stdev(x))
-        n_values.append(int(len(x))/repetitions)
+        n_values.append(int(len(x)))
 
     for stdDev, n in zip(std_dev_delay_rho, n_values):
         errors.append(stdDev/math.sqrt(n)*z)
 
-    plt.errorbar(rho, mean_delay_rho, errors, linestyle='--', marker='*')
+    return mean_delay_rho, errors
+
+
+#-------------------#
+#       MAIN        #
+#-------------------#
+ 
+def main():
+    numberOfRRH = [2, 5, 10]
+    d, n = parseCommand(numberOfRRH)
+    if d is None:
+        print('python plot.ly -d [exp|log] -n [2|5|10]')
+        sys.exit(0)
+            
+    rho = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+    delay_mean = []
+    delay_error = []
+
+    if n == 'all':
+        for x in numberOfRRH:
+            means, errors = getDataForPlotting(d, x)
+            if means:
+                delay_mean.append(means)
+                delay_error.append(errors)
+    else:
+        means, errors = getDataForPlotting(d, n)
+        delay_mean.append(means)
+        delay_error.append(errors)
+
+    #print(delay_mean)
+    #print(delay_error)
+
+    for mean, error in zip(delay_mean, delay_error):
+        plt.errorbar(rho, mean, error, linestyle='--', marker='*', markeredgecolor='red')
+
     plt.xlabel('rho')
     plt.ylabel('Delay (s)')
     plt.title('Mean delay depending on utilization')
