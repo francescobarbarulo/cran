0576af1add041fcc31b8e5441cb4356a77f85afe
francesco.barbarulo <barbarulofrancesco@gmail.com>
2019-01-31 17:40:26 +0100

    Aggiornato script per grafici caso B

--- scripts/b.py
+++ scripts/b.py
@@ -12,29 +12,30 @@ import matplotlib.pyplot as plt
 def parseCommand():
 	const = ['s', 'c', 'n']
 
-	if (len(sys.argv) != 5):
+	if len(sys.argv) != 5:
 		sys.exit()
 
-	if (sys.argv[1] != '-name' or sys.argv[3] != '-const' or sys.argv[4] not in const):
+	if sys.argv[1] != '-name' or sys.argv[3] != '-const' or sys.argv[4] not in const:
 		sys.exit()
 
 	return sys.argv[2], sys.argv[4]
 
 def setAxes(const):
-	speed = [6667, 3333, 2222, 1667, 1333, 1111, 952, 833, 741]
-	compression = [10, 30, 50, 70, 90]
-	#compression = [0, 10, 30, 50]
+	speed = [741, 833, 952, 1111, 1333, 1667, 2222, 3333, 6667]
+	#x = [{'rho': 0.1, 'speed': 6667}, {'rho': 0.2, 'speed': 3333}, {'rho': 0.3, 'speed': 2222}, {'rho': 0.4, 'speed': 1667}, {'rho': 0.5, 'speed': 1333}, {'rho': 0.6, 'speed': 1111}, {'rho': 0.7, 'speed': 952}, {'rho': 0.8, 'speed': 833}, {'rho': 0.9, 'speed': 741}]
+	compression = [10, 20, 30, 40, 50, 70, 90]
+	#compression = [10, 30, 50, 70, 90]
 	#numberOfRrh = [2, 3, 4, 5, 6, 10, 20, 30, 50]
-	numberOfRrh = [2, 3, 4, 5, 6, 10]
+	numberOfRrh = [4, 5, 6, 7, 8, 9, 10, 20, 30, 50]
 
 	if const == 's':
-		return numberOfRrh, compression, 'waitingTimeStat:mean', 'Compression percentage (%)', 'waitingTime (s)' 'Number of Rrhs'
+		return numberOfRrh, compression, 'responseTimeStat:mean', 'Compression percentage (%)', 'RRH Response Time (s)', 'Number of Rrhs'
 
 	elif const == 'n':
-		return speed, compression, 'delayStat:mean', 'Compression percentage (%)', 'Delay (s)', 'Speeds'
+		return speed, compression, 'responseTimeBbuStat:mean', 'Compression percentage (%)', 'Delay (s)', 'Speeds (bytes/s)'
 
 	else:
-		return speed, numberOfRrh, 'delayStat:mean', 'Number of Rrhs', 'Delay(s)', 'Speeds'
+		return speed, numberOfRrh, 'responseTimeStat:mean', 'Number of Rrhs', 'Delay(s)', 'Speeds (bytes/s)'
 
 def main():
 	name, const = parseCommand()
@@ -61,13 +62,18 @@ def main():
 			except Exception, e:
 				print e
 
+		print '%.2f - %.2f' % (y[0], y[-1])
 		plt.errorbar(x, y, yerr=yerr, fmt="-*", markeredgecolor='red', capsize=4, linewidth=1)
 
 
 	plt.xlabel(xlabel)
 	plt.ylabel(ylabel)
+	#plt.yscale('log')
 	plt.grid(linestyle='--')
-	plt.legend(line, title=legendTitle, loc='upper right')
+	#plt.ylim(0, 12)
+	plt.yticks(np.arange(0, 12, 2))
+	#plt.legend(line, title=legendTitle, loc=2, bbox_to_anchor=(1.05, 1), borderaxespad=0.)
+	plt.legend(line, title=legendTitle)
 	plt.show()
 
 
